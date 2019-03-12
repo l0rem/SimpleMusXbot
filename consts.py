d1c3c62793bfc4bd69c7d25fe1ec7e2b8fa46049
l0rem <l0rems@yandex.ru>
2019-03-12 13:30:04 +0300

    Added consts.py

---
+++ consts.py
@@ -0,0 +1,8 @@
+url = 'https://wqo.mp3-tut.online/search?query='
+token = 'YOUR_BOT_TOKEN'
+
+song_form = '''<b>Performer:</b> <i>{}</i>
+<b>Title:</b> <i>{}</i>
+<b>Duration:</b> <i>{}</i>'''
+
+storage_cid = -100000 #chat_id of your "storage"-chat. bot must be in it.
